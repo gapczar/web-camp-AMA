418d04265b72f9e178ec15cfdd502cfb7fe97ba5
GAPLabs <GAPLabs@neri.fundmytravel.lan>
2013-02-24 14:55:25 +0800

    bow

--- web/pages/getAllapps.php
+++ web/pages/getAllapps.php
@@ -7,6 +7,8 @@
 		while($row = mysql_fetch_row($query)){
 			echo("<tr>");
 					echo("<td><input type='checkbox' id='checker' value='$row[0]'></td>");
+					echo("<td><img src='../../apps-list/images/$row[1]' width='124px' height='124px'></td>");
+					echo("<td>$row[0]</td>");
 				echo("</tr>");
 		}
 	}
