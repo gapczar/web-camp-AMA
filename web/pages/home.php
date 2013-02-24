b36402eb2489ee789292239845ca22ac1babac81
Clyde Alegro <clyde.alegro@gmail.com>
2013-02-24 09:15:07 +0800

    add these web folders

---
+++ web/pages/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Wendy+One' rel='stylesheet' type='text/css'>
+		<link href='../css/index.css' rel='stylesheet' type='text/css'>
+		<title>Poll Website for Mobile Apps</title>
+	</head>
+	<body>
+		<div class="divBody" >
+			<header>
+				<h1>Poll for Mobile APPS</h1>
+			</header>
+			<section>
+				
+			</section>
+			<footer>
+				
+			</footer>
+		</div>
+	</body>
+</html>
